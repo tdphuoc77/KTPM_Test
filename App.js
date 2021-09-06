c42c2b7151ec5138bca9552aad1fff7c6ddae316
Phuoc <tdphuoc77@gmail.com>
2021-09-06 11:03:56 +0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Image, Platform, StyleSheet, Switch, View } from 'react-native';
+import bat from "./assets/c02n43451co3o9rj1ee0.png"
+import tat from "./assets/tat.png"
+import Constants from 'expo-constants'
+import { Dimensions } from 'react-native';
 
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 export default function App() {
+  // const os = Platform.OS
+
+  const [isEnabled, setIsEnabled] = useState(false);
+
+  const [isImage, setImage] = useState(tat);
+
+  const toggleSwitch = () => {
+    if (isEnabled) {
+      setImage(tat)
+    } else setImage(bat)
+    setIsEnabled(previousState => !previousState)
+
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <View style={styles.content}>
+
+        <Image source={isImage} style={styles.image} />
+
+        <Switch
+          trackColor={{ false: "#767577", true: "#81b0ff" }}
+          thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+          ios_backgroundColor="#3e3e3e"
+          onValueChange={toggleSwitch}
+          value={isEnabled}
+        />
+      </View>
+
+
     </View>
   );
 }
@@ -17,5 +52,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight
   },
+
+  content: {
+
+  },
+
+  image: {
+    // maxHeight: { height },
+    // maxWidth: '100%',
+    resizeMode: 'contain',
+    width: windowWidth,
+    height: windowHeight - 150,
+  }
 });
