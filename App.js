3afd604705e154ed3bd256d5c9e9ed42d4798a59
Phuoc <tdphuoc77@gmail.com>
2021-09-06 11:13:19 +0700

    test

--- App.js
+++ App.js
@@ -35,7 +35,7 @@ export default function App() {
         <Switch
           trackColor={{ false: "#767577", true: "#81b0ff" }}
           thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
-          ios_backgroundColor="#3e3e3e"
+          ios_backgroundColor="#3e3e3d"
           onValueChange={toggleSwitch}
           value={isEnabled}
         />
